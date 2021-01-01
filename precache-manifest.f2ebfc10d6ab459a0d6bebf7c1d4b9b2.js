db65f1f22d51abca01e2e9ee2e7af015eb73a424
Тимур <180194@gmail.com>
2021-01-01 20:15:33 +0300

    Updates

---
+++ precache-manifest.f2ebfc10d6ab459a0d6bebf7c1d4b9b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ca2809f6a58d21c108ae8add204a9ba",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "de09e751a0f3aa1cf2eb",
+    "url": "/react-todo-app/static/css/main.dc5ab7dd.chunk.css"
+  },
+  {
+    "revision": "75cb94d9cb55c7934c06",
+    "url": "/react-todo-app/static/js/2.7af93ab0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-todo-app/static/js/2.7af93ab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de09e751a0f3aa1cf2eb",
+    "url": "/react-todo-app/static/js/main.6d620cfc.chunk.js"
+  },
+  {
+    "revision": "5d8ab65282b28fa01c14",
+    "url": "/react-todo-app/static/js/runtime-main.60754366.js"
+  }
+]);
